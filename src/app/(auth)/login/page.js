6802ee53d67e08cceab0a21ea890ad97892daf90
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-24 18:49:51 +0530

    div

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -4,11 +4,9 @@ const Loginpage = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="w-[500px] bg-stone-800  p-12 flex flex-col text-center gap-7 rounded-md">
-        <form>
-        <h1>Login With Github</h1>
+        <h1>Login With Github</h1></div>
             <LoginForm/>
-            </form>
-        </div>
+        
     </div>
   )
 }
