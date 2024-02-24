89d6a43cd2487a890a2186e2fb8a25459e333b32
A.Yasmin Banu <151750381+DeveloperYasmin@users.noreply.github.com>
2024-02-24 18:29:58 +0530

    Update page.js

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,13 +1,13 @@
 import LoginForm from "@/app/components/loginForm/loginForm"
-import { handleGithubLogin } from "@/app/lib/action"
+
 
 
 const Loginpage = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="w-[500px] bg-stone-800  p-12 flex flex-col text-center gap-7 rounded-md">
-      <form action={handleGithubLogin}>
-        <button className="w-[100%] p-5 cursor-pointer text-lg">Login With Github</button>
+
+        <h1>Login With Github</h1>
       </form>
       <LoginForm/>
     </div></div>
