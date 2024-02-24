660c7030aa9606b95e60a9629883db54d059bb0c
DeveloperYasmin <banuyasmin96@gmail.com>
2024-02-24 18:54:46 +0530

    div

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -2,12 +2,9 @@ import LoginForm from "@/app/components/loginForm/loginForm"
 
 const Loginpage = () => {
   return (
-    <div className="flex items-center justify-center">
-      <div className="w-[500px] bg-stone-800  p-12 flex flex-col text-center gap-7 rounded-md">
-        <h1>Login With Github</h1></div>
-            <LoginForm/>
-        
-    </div>
+      <div className="w-[500px] items-center justify-center bg-stone-800  p-12 flex flex-col text-center gap-7 rounded-md">
+            <LoginForm/>  
+      </div>
   )
 }
 
