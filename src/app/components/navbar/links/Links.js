ee9c01915747ab572335c6e41841e9a2a0be616b
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-05 07:45:12 +0530

    menu

--- src/app/components/navbar/links/Links.js
+++ src/app/components/navbar/links/Links.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import Navlink from '../navlink/navlink'
 import Image from 'next/image'
 import { handleLogout } from '@/app/lib/action'
@@ -27,7 +27,13 @@ import { handleLogout } from '@/app/lib/action'
 
     const Links =({session})=>{
         const [open,setopen]=useState(false)
-    
+    const Menuref=useRef()
+    const Imgref=useRef()
+    window.addEventListener("click",(e)=>{
+        if(e.target !== Menuref.current && e.target !== Imgref.current){
+            setopen(false) 
+        }
+    })
     //TEMPORARY
     const isAdmin=true
   return (
@@ -47,12 +53,21 @@ import { handleLogout } from '@/app/lib/action'
         <Navlink item={{title:"Login",path:"/login"}}/>
         )}
     </div>
-    <Image className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen((prev)=>!prev)}></Image>
+    <Image ref={Imgref} className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen(!open)}></Image>
     {
-        open && (<div className='absolute z-20 h-[85%] rounded-xl top-24 lg:hidden right-0 w-[50%] flex flex-col items-center justify-center gap-3  bg-stone-900'>
-    {links.map((link)=>(<Navlink item={link} key={link.title}/>))}
-    <Navlink item={{title:"Admin",path:"/admin"}}/>
+        open && (<div ref={Menuref} onClick={()=>setopen(false)} className='absolute z-20 h-[85%] rounded-xl top-24 lg:hidden right-0 w-[50%] flex flex-col items-center justify-center gap-3  bg-stone-900'>
+    {links.map((link=>(
+            <Navlink  item={link} key={link.title}/>
+        )))}{session?.user?(
+            <>      
+            {session?.user?.isAdmin && <Navlink item={{title:"Admin",path:"/admin"}}/>}
+            <form action={handleLogout}>
              <button className='bg-yellow-600 rounded-md p-3 m-2 text-black font-bold'>Logout</button>
+             </form>
+             </>
+        ):(
+            <Navlink item={{title:"Login",path:"/login"}}/>
+            )}
     </div>
     )}
     </div>
