278f4a52ac0f786beb024a1cc2d30cd8ec235438
DeveloperYasmin <banuyasmin96@gmail.com>
2024-03-05 08:37:32 +0530

    menu

--- src/app/components/navbar/links/Links.js
+++ src/app/components/navbar/links/Links.js
@@ -1,79 +1,16 @@
-"use client"
-import { useRef, useState } from 'react'
-import Navlink from '../navlink/navlink'
-import Image from 'next/image'
-import { handleLogout } from '@/app/lib/action'
+import Link from "next/link"
 
-
-
-    const links = [
-        {
-            title:"Home",
-            link:"/"
-        },
-        {
-            title:"About",
-            path:"/about"
-        },
-        {
-            title:"Contact",
-            path:"/contact"
-        },
-        {
-            title:"Blog",
-            path:"/blog"
-        },
-        
-    ]
-
-    const Links =({session})=>{
-        const [open,setopen]=useState(false)
-    const Menuref=useRef()
-    const Imgref=useRef()
-    window.addEventListener("click",(e)=>{
-        if(e.target !== Menuref.current && e.target !== Imgref.current){
-            setopen(false) 
-        }
-    })
-    //TEMPORARY
-    const isAdmin=true
+const Links = () => {
   return (
-    <div>
-    <div className='lg:flex items-center gap-3 hidden'>
-        {links.map((link=>(
-            <Navlink item={link} key={link.title}/>
-        )))}{session?.user?(
-            <>      
-            {session?.user?.isAdmin && <Navlink item={{title:"Admin",path:"/admin"}}/>}
-            <form action={handleLogout}>
-             <button className='bg-yellow-600 rounded-md p-3 m-2 text-black font-bold'>Logout</button>
-             </form>
-             </>
+    <nav>
+        <Link href="/"> <a>Logo</a></Link>
+        <ul>
+            <li><Link legacyBehavior  href="/"><a>Home</a></Link></li>
+            <li><Link legacyBehavior  href="/about"><a>About</a></Link></li>
 
-        ):(
-        <Navlink item={{title:"Login",path:"/login"}}/>
-        )}
-    </div>
-    <Image ref={Imgref} className='lg:hidden cursor-pointer' src="/menu.png" alt=" " width={30} height={30} onClick={()=>setopen(!open)}></Image>
-    {
-        open && (<div ref={Menuref} onClick={()=>setopen(false)} className='absolute z-20 h-[85%] rounded-xl top-24 lg:hidden right-0 w-[50%] flex flex-col items-center justify-center gap-3  bg-stone-900'>
-    {links.map((link=>(
-            <Navlink  item={link} key={link.title}/>
-        )))}{session?.user?(
-            <>      
-            {session?.user?.isAdmin && <Navlink item={{title:"Admin",path:"/admin"}}/>}
-            <form action={handleLogout}>
-             <button className='bg-yellow-600 rounded-md p-3 m-2 text-black font-bold'>Logout</button>
-             </form>
-             </>
-        ):(
-            <Navlink item={{title:"Login",path:"/login"}}/>
-            )}
-    </div>
-    )}
-    </div>
+        </ul>
+    </nav>
   )
 }
 
-export default Links 
-
+export default Links
--- src/app/components/navbar/navlink/navlink.js
+++ src/app/components/navbar/navlink/navlink.js
@@ -1,13 +1,16 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import Links from "../links/Links"
 
 const Navlink = ({item}) => {
 
     const pathName=usePathname()
   return (
     <div className="min-w-24 p-3  font-medium text-center">
-           <Link href={item.link ?? "/"} className={`${pathName===item.path && " bg-yellow-600 text-black rounded-full m-2 p-4" }`} >{item.title}</Link>
+      <Links/>
+      <h1>Home</h1>
+     <h1>About</h1>
 
     </div>
   )
