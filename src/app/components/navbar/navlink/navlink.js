4c90099358f81f9a5c04f9e8be3d2151e8dc3f60
A.Yasmin Banu <151750381+DeveloperYasmin@users.noreply.github.com>
2024-03-07 15:58:21 +0530

    Update navlink.js

--- src/app/components/navbar/navlink/navlink.js
+++ src/app/components/navbar/navlink/navlink.js
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import styles from "./navLink.module.css";
 import { usePathname } from "next/navigation";
 
 const NavLink = ({ item }) => {
@@ -11,7 +10,7 @@ const NavLink = ({ item }) => {
     <Link
       href={item.path}
       className={` ${
-        pathName === item.path && styles.active
+        pathName === item.path 
       }`}
     >
       {item.title}
